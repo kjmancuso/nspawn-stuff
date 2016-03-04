7a936b57cf0bea5afccfd03c54bbd6780ab7f909
Kevin Mancuso <kevin@mancu.so>
2016-03-03 21:18:03 -0600

    adding nspawn-gen.sh

---
+++ nspawn-gen.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Site Config
+DISTRO=$1
+NAME=$2
+CONTAINER_DIR=/var/lib/container/
+FULL_PATH=$CONTAINER_DIR/$NAME/
+DEBOOTSTRAP_SCRIPTS=/usr/share/debootstrap/scripts/
+NETIF=eth1
+
+# Container specific values
+MACADDR=$(echo $NAME | md5sum | sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
+
+if [ $# -eq 0 ]; then
+    echo "usage: $0 <distro> <container name>"
+    exit 1
+fi
+
+if [ $(id -u) -gt 0 ]; then
+    echo "I think this should be run as root."
+    exit 1
+fi
+
+if [ ! -f $DEBOOTSTRAP_SCRIPTS/$1 ]; then
+    echo "Debootstrap script $1 does not exist"
+    exit 1
+fi
+
+
+echo "Beginning to bootstrap $1 to $FULL_PATH."
+/usr/sbin/debootstrap $1 $FULL_PATH
+echo "Symlinking resolvconf"
+ln -sf $FULL_PATH/run/systemd/resolve/resolv.conf $FULL_PATH/etc/resolv.conf
+echo "Creating $FULL_PATH/etc/systemd/network/mv-$NETIF.network"
+cat > $FULL_PATH/etc/systemd/network/mv-$NETIF.network << EOF
+[Match]
+Name=mv-eth1
+
+[Network]
+DHCP=yes
+
+[Link]
+MACAddress=$MACADDR
+
+EOF
+
+    /usr/bin/systemd-nspawn --directory=$FULL_PATH systemctl enable systemd-networkd.service systemd-resolved.service
+
+    echo "Creating /etc/systemd/system/container-$NAME.service"
+    cat > /etc/systemd/system/container-$NAME.service << EOF
+[Unit]
+Description=Container $NAME
+
+[Service]
+ExecStart=/usr/bin/systemd-nspawn --quiet --keep-unit --link-journal=try-guest --directory=$FULL_PATH --network-macvlan=$NETIF --boot
+KillMode=mixed
+Type=notify
+RestartForceExitStatus=133
+SuccessExitStatus=133
+
+[Install]
+WantedBy=multi-user.target
+EOF
